d9de29bc737128d3ccfe2e9b107e5d43aa18b311
1ixi1 <syshchenko228@gmail.com>
2021-09-26 13:37:24 +0300

    initial commit

---
+++ FirstApp/main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.show();
+    
+    return a.exec();
+}
